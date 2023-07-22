e2ffcc45f8fc999a255569aa848fc5e54f8db63f
keremsahinoglu <goinpeace.un@outlook.com>
2023-07-22 11:40:45 +0300

    alistirma

---
+++ app.js
@@ -0,0 +1,72 @@
+// Girilen sayının tek mi çift mi olduğunu bulan programı yazınız.
+
+// cevap 
+
+// function tekCift(sayi) {
+//     if (sayi % 2 === 0) {
+//         console.log(sayi + " çift sayıdır");
+//     } else {
+//         console.log(sayi + " tek sayıdır");
+//     }
+// }
+
+// let girilenSayi = prompt("Sayı giriniz.");
+// tekCift(girilenSayi);
+
+// Girilen sayı 4, 5 ve 6 sayılarından en az birine bölünüyorsa “Sayı Uygun”, aksi halde “Sayı Uygun Değil” mesajı veren programı yazınız.
+
+// cevap
+
+
+// let sayi = prompt("sayi gir");
+
+// if  (sayi % 4==0 || sayi % 5 ==0 || sayi % 6==0) {
+//     console.log("SAYI OKEEEYYY")
+// } 
+    
+// else {
+//     console.log("SAYI OKEEEYYY DEĞİİİLLLL")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Girilen sayı 0 ile 100 arasında ve çift ise “Sayı Uygun”, aksi halde “Sayı Uygun Değil” mesajı veren programı yazınız.
+
+
+
+// cevap
+
+// let sayi = prompt("sayi giriniz")
+
+// if (sayi > 0 && sayi <= 100 && sayi % 2 === 0 ) {
+//     console.log("sayi uygundur efenim")
+// }
+// else {
+//     console.log("sayı uygun değil")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
